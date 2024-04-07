6c7fbd070032ab537f3f49298f28e507b0bc9b1e
grdddj <jiri.musil06@seznam.cz>
2024-04-07 16:00:54 +0200

    chore: move file reading function to common

--- auth_monitor.py
+++ auth_monitor.py
@@ -1,16 +1,12 @@
-import os
 import re
-import time
 from pathlib import Path
-from typing import Iterator
 
+from common import yield_file_lines
 from config import load_config
 from logger import get_json_logger
 from notify import send_pushbullet_message
 
 logger = get_json_logger(Path(__file__).with_suffix(".log"))
-
-HERE = Path(__file__).parent
 
 FILE = "/var/log/auth.log"
 
@@ -27,42 +23,8 @@ unauthorized_pattern = re.compile(
 )
 
 
-def yield_auth_log_lines(file_path: str) -> Iterator[str]:
-    """Monitor /var/log/auth.log for new login attempts and authentications, accounting for log rotation."""
-    try:
-        with open(file_path, "r") as file:
-            # Initially, move to the end of the file
-            file.seek(0, 2)
-            last_known_size = file.tell()
-
-            while True:
-                current_size = os.path.getsize(file_path)
-                if current_size < last_known_size:
-                    # File was truncated/rotated, move to the beginning
-                    logger.info(
-                        {
-                            "event": "FileRotated",
-                            "size": current_size,
-                            "last_known_size": last_known_size,
-                        }
-                    )
-                    file.seek(0)
-                last_known_size = current_size
-
-                line = file.readline().strip()
-                if not line:
-                    time.sleep(0.1)  # Wait for new content
-                    continue
-
-                yield line
-    except FileNotFoundError:
-        logger.error({"event": "FileNotFound", "file": file_path})
-    except PermissionError:
-        logger.error({"event": "PermissionError", "file": file_path})
-
-
 def monitor_and_report() -> None:
-    for line in yield_auth_log_lines(FILE):
+    for line in yield_file_lines(FILE, logger=logger):
         try:
             analyze_line(line)
         except Exception as e:
---
+++ common.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import time
+from typing import Iterator
+
+
+def yield_file_lines(file_path: str, logger: logging.Logger) -> Iterator[str]:
+    """Monitor /var/log/auth.log for new login attempts and authentications, accounting for log rotation."""
+    try:
+        with open(file_path, "r") as file:
+            # Initially, move to the end of the file
+            file.seek(0, 2)
+            last_known_size = file.tell()
+
+            while True:
+                current_size = os.path.getsize(file_path)
+                if current_size < last_known_size:
+                    # File was truncated/rotated, move to the beginning
+                    logger.info(
+                        {
+                            "event": "FileRotated",
+                            "size": current_size,
+                            "last_known_size": last_known_size,
+                        }
+                    )
+                    file.seek(0)
+                last_known_size = current_size
+
+                line = file.readline().strip()
+                if not line:
+                    time.sleep(0.1)  # Wait for new content
+                    continue
+
+                yield line
+    except FileNotFoundError:
+        logger.error({"event": "FileNotFound", "file": file_path})
+    except PermissionError:
+        logger.error({"event": "PermissionError", "file": file_path})
